7d73e6afb33baf2a4d57631a9005bbf538ecd123
LIXIAOLI <29590847@qq.com>
2021-03-16 19:29:10 +0800

    all

---
+++ config/defaultSettings.js
@@ -0,0 +1,14 @@
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'top',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '办公物品管理系统',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
